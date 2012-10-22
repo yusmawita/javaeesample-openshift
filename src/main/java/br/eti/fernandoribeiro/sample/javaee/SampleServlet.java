9cacd2cd78945a7877ba2b8246f6f0d353a83e8f
fribeiro1 <webmaster@fernandoribeiro.eti.br>
2012-10-22 18:14:01 -0200

    Replace serialVersionUID with @SuppressWarnings("serial")

--- src/main/java/br/eti/fernandoribeiro/sample/javaee/SampleServlet.java
+++ src/main/java/br/eti/fernandoribeiro/sample/javaee/SampleServlet.java
@@ -20,9 +20,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 @WebServlet(name = "SampleServlet", value = "/sendMessage")
 public final class SampleServlet extends HttpServlet {
-	private static final long serialVersionUID = -9144205199226729308L;
 
 	@Override
 	protected void doGet(final HttpServletRequest req,
